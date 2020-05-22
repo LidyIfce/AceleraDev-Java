3f377bc5056ab2ec34bcf0a6368165d285f9200b
lidy_gb <lidianegomesbarbosa@gmail.com>
2020-05-22 13:50:49 -0300

    Modulo 4 desafio java-10

---
+++ quarto-modulo/java-10/src/main/java/com/challenge/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.challenge.repository;
+
+import com.challenge.entity.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository <User, Long>{
+
+    @Query("select c.id.user from Candidate c where c.id.acceleration.name like:name")
+    List<User> findByAccelerationName(String name);
+    @Query("select c.id.user from Candidate c where c.id.company.id = :companyId")
+    List<User> findByCompanyId(Long companyId);
+
+}
